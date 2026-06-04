c2cb00a0232d8d2f63252cb1bbe24a29529a18d2
Ujjawal Thakre <thakreujjawal@gmail.com>
2026-06-04 22:45:42 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,24 @@
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+print("1. Add")
+print("2. Subtract")
+print("3. Multiply")
+print("4. Divide")
+
+choice = input("Choose operation: ")
+
+if choice == "1":
+    print("Result =", num1 + num2)
+
+elif choice == "2":
+    print("Result =", num1 - num2)
+
+elif choice == "3":
+    print("Result =", num1 * num2)
+
+elif choice == "4":
+    print("Result =", num1 / num2)
+
+else:
+    print("Invalid Choice")
