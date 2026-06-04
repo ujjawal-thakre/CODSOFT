c43a4c3f2f84d6a27543a89d22e610ee38e3767b
Ujjawal Thakre <thakreujjawal@gmail.com>
2026-06-04 22:44:27 +0530

    Add files via upload

---
+++ todo.py
@@ -0,0 +1,24 @@
+tasks = []
+
+while True:
+
+    print("\n1.Add Task")
+    print("2.View Tasks")
+    print("3.Exit")
+
+    choice = input("Enter choice: ")
+
+    if choice == "1":
+        task = input("Enter task: ")
+        tasks.append(task)
+
+    elif choice == "2":
+        print("\nTasks:")
+        for task in tasks:
+            print(task)
+
+    elif choice == "3":
+        break
+
+    else:
+        print("Invalid Choice")
